6c4d9bd1cbb635f4a0f92c6301d362de15298f84
Vasil Yanakiev <>
2021-06-11 00:42:28 -0700

    update

--- solution.c
+++ solution.c
@@ -3,26 +3,16 @@
 #include<stdio.h>
 int main(int argc, char *argv[]) {
    printf("%d",argc);
-   char firstCommand[20],secondCommand[20],fileName[20];
-   strcpy(firstCommand,argv[1]);
-   strcpy(secondCommand,argv[2]);
-   strcpy(fileName,argv[3]);
-   FILE *fp = fopen(fileName, "ab+");
-   if(system(firstCommand)==-1){
-      printf("Command \"%s\" failed!", firstCommand);
-   }else{
-      char toAppend[21];
-      strcat(toAppend,firstCommand);
-      strcat(toAppend,"\n");
-      fputs(toAppend,fp);
-   }
-   if(system(secondCommand)==-1){
-      printf("Command \"%s\" failed!", secondCommand);
-   }else{
-      char toAppend[21];
-      strcat(toAppend,secondCommand);
-      strcat(toAppend,"\n");
-      fputs(toAppend,fp);
+   FILE *fp = fopen(argv[3], "ab+");
+   for(int i =1;i<argc-1;i++){
+      if(system(argv[i])==-1){
+         printf("Command \"%s\" failed!", argv[i]);
+      }else{
+         char toAppend[21];
+         strcat(toAppend,argv[i]);
+         strcat(toAppend,"\n");
+         fputs(toAppend,fp);
+      }
    }
    printf("\n");
    fclose(fp);
