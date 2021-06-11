81a6ab95cd26c132ca571fa1578642edacf6ead0
Vasil Yanakiev <>
2021-06-11 01:43:57 -0700

    push

--- solution.c
+++ solution.c
@@ -4,8 +4,8 @@
 int main(int argc, char *argv[]) {
    FILE *fp = fopen(argv[3], "ab+");
    for(int i =1;i<argc-1;i++){
-      int returnCode=system(argv[i])==-1;
-      if(returnCode==-1){
+      int returnCode=system(argv[i]);
+      if(returnCode!=0){
          printf("Command \"%s\" failed!", argv[i]);
          printf("\n");
       }else{
@@ -13,6 +13,7 @@ int main(int argc, char *argv[]) {
          strcat(toAppend,argv[i]);
          strcat(toAppend,"\n");
          fputs(toAppend,fp);
+         toAppend[0]='\0';
       }
    }
    fclose(fp);
